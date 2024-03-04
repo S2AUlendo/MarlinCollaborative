2249ead968623079af18d3ae3f3069f39431c9be
narno2202 <narno2202@gmail.com>
2024-03-04 23:27:37 +0100

    Set MINIMUM_STEPPER_POST_DIR_DELAY to 100ns Add axis_did_move.reset() in discard_planner_block_protected() to solve BLTouch HS mode issue

--- Marlin/src/module/ft_motion.cpp
+++ Marlin/src/module/ft_motion.cpp
@@ -405,6 +405,7 @@ void FTMotion::discard_planner_block_protected() {
                                 // be null when the "block" is a runout or generated) in order
                                 // to use planner.release_current_block(). 
     stepper.current_block = nullptr;
+    stepper.axis_did_move.reset();
     planner.release_current_block();  // FTM uses release_current_block() instead of discard_current_block(),
                                       // as in block_phase_isr(). This change is to avoid invoking axis_did_move.reset().
                                       // current_block = nullptr is added to replicate discard without axis_did_move reset.
